a290e85fef1b146ea9ab5b6f6bf943a36a7c4622
Martivial <mateuszrev@gmail.com>
2023-10-13 09:44:33 +0200

    k

---
+++ zadnie1_emerytura.cs
@@ -0,0 +1,32 @@
+﻿
+
+string surname = Console.ReadLine();
+var age = int.Parse(Console.ReadLine());
+var age_pension = int.Parse(Console.ReadLine());
+var calc = (age_pension - age);
+Convert.ToInt32(calc);
+
+
+Console.WriteLine($"Witaj, {surname}!");
+
+if (age < 0 || age_pension < 0)
+{
+    Console.WriteLine("Wiek nie może być ujemny!");
+}
+else
+{
+
+    if (age < age_pension)
+ 
+    {
+
+        Console.WriteLine($"Do emerytury brakuje Ci {calc} lat!");
+    }
+    else
+    {
+        Console.WriteLine("Jesteś emerytem!");
+    }
+}
+
+
+
